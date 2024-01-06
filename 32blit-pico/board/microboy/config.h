facdff332a13f38a9599937be401c32e35a8edd2
cpasjuste <cpasjuste@gmail.com>
2024-01-06 19:04:22 +0100

    microboy: add v1.1 board support

--- 32blit-pico/board/microboy/config.h
+++ 32blit-pico/board/microboy/config.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#define ALLOW_HIRES 0
+
+#define MICROBOY_11
+
+#if defined(MICROBOY_10)
 // "NEOPIXEL" led
 #define LED_WS2812_PIN 16
 
@@ -11,10 +16,26 @@
 #define LCD_BACKLIGHT_PIN 12
 #define LCD_RESET_PIN 13
 
-// spi
+// sd spi
 #define SD_SCK  18
 #define SD_MOSI 19
 #define SD_MISO 20
 #define SD_CS   6
+#elif defined(MICROBOY_11)
+
+// st7789 240x240
+#define LCD_RESET_PIN 4
+#define LCD_CS_PIN 5
+#define LCD_SCK_PIN 6
+#define LCD_MOSI_PIN 7
+#define LCD_DC_PIN 8
+#define LCD_BACKLIGHT_PIN 9
+
+#define SD_SCK  13
+#define SD_MOSI 14
+#define SD_MISO 15
+#define SD_CS   2
+
+#endif
 
 #define USB_PRODUCT_STR "MicroBoy"
